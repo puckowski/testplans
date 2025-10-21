ada79849fa024c8199421a06dcc778ba59abfcd8
Daniel Puckowski <puckowski.d@gmail.com>
2025-10-21 18:31:23 -0400

    fix: update afterburner configuration
    * Fix Afterburner configuration for Jackson for performance.

--- testing/src/main/java/com/puckowski/testing/config/JacksonAfterburnerConfig.java
+++ testing/src/main/java/com/puckowski/testing/config/JacksonAfterburnerConfig.java
@@ -1,5 +1,6 @@
 package com.puckowski.testing.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,16 @@ public class JacksonAfterburnerConfig {
 
     @Bean
     public AfterburnerModule afterburnerModule() {
-        AfterburnerModule module = new AfterburnerModule();
+        AfterburnerModule module = new AfterburnerModule().setUseValueClassLoader(true);
         return module;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        mapper.registerModule(new AfterburnerModule()
+                .setUseValueClassLoader(true));
+        return mapper;
     }
 }
